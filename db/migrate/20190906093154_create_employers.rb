cd3b4812ceaa5d85ee721bc90bb004efda827f5b
inanBekir <bbekirinan@gmail.com>
2019-09-06 12:05:37 +0200

    Initial Commit

---
+++ db/migrate/20190906093154_create_employers.rb
@@ -0,0 +1,10 @@
+class CreateEmployers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :employers do |t|
+      t.string :name
+      t.string :surname
+
+      t.timestamps
+    end
+  end
+end
